15e4be6c10e9a55a24469e7e221c8a658577b1b2
Shima Ryuhei <65934663+islandryu@users.noreply.github.com>
2025-11-26 20:22:31 +0900

    wip

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,14 @@
+return {
+  { "folke/tokyonight.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,15 +1,22 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-    picker = {
+  opts = function(_, opts)
+    opts.picker = opts.picker or {}
+    opts.picker.sources = opts.picker.sources or {}
+
+    opts.picker.exclude = { -- add folder names here to exclude
+        ".git",
+        "node_modules",
+    }
+
+    -- 🔹 既存 files 設定を壊さずに hidden / ignored を上書き
+    opts.picker.sources.files = vim.tbl_deep_extend("force", opts.picker.sources.files or {}, {
       hidden = true,
       ignored = true,
-      sources = {
-        files = {
-          hidden = true,
-          ignored = true,
-        },
-      },
-    },
-  },
+      -- ここに args はもう入れない！ (--glob は find には通用しない)
+    })
+
+
+    return opts
+  end,
 }
